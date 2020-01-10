55fb967f240f9aae8b01ff2e0801756f2b70dba0
VasylievHennadii <vga.kha@gmail.com>
2020-01-10 17:00:31 +0200

    first commit

--- 8kyu_BasicMathematicalOperations.php
+++ 8kyu_BasicMathematicalOperations.php
@@ -1,9 +1,7 @@
 <?php
 // Your task is to create a function that does four basic mathematical operations.
-
 // The function should take three arguments - operation(string/char), value1(number), value2(number).
 // The function should return result of numbers after applying the chosen operation.
-
 // Examples
 // basicOp('+', 4, 7)         // Output: 11
 // basicOp('-', 15, 18)       // Output: -3
