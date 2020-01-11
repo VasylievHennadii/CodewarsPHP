819f7e99ce33fe63d153884aed05e942bde3f437
VasylievHennadii <vga.kha@gmail.com>
2020-01-11 12:18:54 +0200

    Multiples of 3 or 5

---
+++ 6kyu_MultiplesOf3Or5.php
@@ -0,0 +1,23 @@
+<?php
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+
+// first option
+function solution($number){
+$sum = 0;
+    for ($i = 3; $i < $number; $i++) {
+        if ($i % 3 === 0 || $i % 5 === 0) {
+            $sum += $i;
+        }
+    }  return $sum;
+}
+
+
+// second option
+function solution($number){
+return array_sum(array_filter(range(1, $number - 1), function($n) {return $n % 3 == 0 or $n % 5 == 0;}));
+}
+
+?>
