fa3720199ce4dfc66fae796eb94e1c486d337714
VasylievHennadii <vga.kha@gmail.com>
2020-01-15 10:06:11 +0200

    Count the monkeys

---
+++ 8kyu_CountTheMonkeys.php
@@ -0,0 +1,26 @@
+<?php
+// You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+// As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+// For example:
+
+// monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// monkeyCount(1) // --> [1]
+
+// first solution
+function monkeyCount($n) {
+    return range(1, $n);
+}
+
+
+// second solution
+function monkeyCount($n) {
+    $arr = array();
+    for( $i = 1; $i <= $n; $i++) {   
+      array_push($arr, $i);
+      }
+    return $arr;  
+}
+
+?>
